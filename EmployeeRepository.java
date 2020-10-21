ed9460cb7182f61ebbe31d17b64c80ef41c6a3a4
sruthiCG <66354136+sruthiCG@users.noreply.github.com>
2020-10-21 11:50:16 +0530

    Delete EmployeeRepository.java

--- EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.src.java.EmpProj;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-}
