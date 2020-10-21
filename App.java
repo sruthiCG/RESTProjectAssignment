d9eaa97a61844af37295940a996c083eeed4b382
sruthiCG <66354136+sruthiCG@users.noreply.github.com>
2020-10-21 11:45:00 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.src.java.EmpProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot Main class
+ **/
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
